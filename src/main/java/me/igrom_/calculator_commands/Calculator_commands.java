92a4779b5c4d2b294a5abf64c0bec7987dec24f6
Grom <93106270+lGrom@users.noreply.github.com>
2024-01-13 11:55:47 -0600

    Initialized repository

---
+++ src/main/java/me/igrom_/calculator_commands/Calculator_commands.java
@@ -0,0 +1,13 @@
+package me.igrom_.calculator_commands;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Calculator_commands implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
