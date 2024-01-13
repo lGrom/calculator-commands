57fedefd4ec6505a14f992b1803c873875b09f63
Grom <person.japescapes@gmail.com>
2024-01-13 11:55:47 -0600

    Initialized repository

---
+++ src/main/java/me/igrom_/calculator_commands/client/Calculator_commandsClient.java
@@ -0,0 +1,13 @@
+package me.igrom_.calculator_commands.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Calculator_commandsClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
