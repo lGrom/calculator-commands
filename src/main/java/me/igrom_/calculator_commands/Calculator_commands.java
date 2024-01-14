4a3177ffafaba48bd8e31dcaac4a44569426cc00
Grom <person.japescapes@gmail.com>
2024-01-13 21:07:27 -0600

    Added basic command

--- src/main/java/me/igrom_/calculator_commands/Calculator_commands.java
+++ src/main/java/me/igrom_/calculator_commands/Calculator_commands.java
@@ -1,6 +1,12 @@
 package me.igrom_.calculator_commands;
 
+import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.minecraft.text.Text;
+
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
 
 public class Calculator_commands implements ModInitializer {
     /**
@@ -8,6 +14,14 @@ public class Calculator_commands implements ModInitializer {
      */
     @Override
     public void onInitialize() {
-
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(
+            ClientCommandManager.literal("calculate")
+            .then(argument("expression", StringArgumentType.greedyString())
+            .executes(context -> {
+                final String expression = StringArgumentType.getString(context, "expression");
+//                final int result = kjlasdf;
+                context.getSource().sendFeedback(Text.literal(expression));
+                return 1;
+        }))));
     }
 }
